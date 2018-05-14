b08f488bbdaa2e296967a6b5719e93aac3330a9d
KEKE <493006007@qq.com>
2018-05-14 11:07:09 +0800

    commit permutations

---
+++ src/solution/Permutations.java
@@ -0,0 +1,39 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Created by KEKE on 2018/5/14
+ */
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> list = new ArrayList<>();
+        trackBack(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private void trackBack(List<List<Integer>> list, List<Integer> tempList, int[] nums){
+
+        if (tempList.size() == nums.length)
+            list.add(new ArrayList<>(tempList));
+        else {
+            for(int i=0;i<nums.length;i++){
+                if (tempList.contains(nums[i]))
+                    continue;
+                tempList.add(nums[i]);
+                trackBack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {1,2,3};
+        System.out.println(new Permutations().permute(arr));
+    }
+}
